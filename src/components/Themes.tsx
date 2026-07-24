da3c249f8246cd97cc338474b61d935592d18685
Amber Joy <amber.joy.dev@gmail.com>
2026-07-24 13:26:24 -0500

    Use native select on mobile, highlight selected item in theme/mode menus

--- src/components/Themes.tsx
+++ src/components/Themes.tsx
@@ -1,4 +1,5 @@
 import {
+  CheckIcon,
   ChevronDownIcon,
   MoonIcon,
   PhoneIcon,
@@ -6,6 +7,7 @@ import {
   TimeIcon,
 } from "@chakra-ui/icons";
 import {
+  Box,
   Button,
   Heading,
   Icon,
@@ -13,15 +15,19 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  Select,
   Stack,
   Text,
 } from "@chakra-ui/react";
 
 import { useDayModePreference } from "../context/DayModePreferenceContext";
 import { useThemeName } from "../context/ThemeNameContext";
-import { DAY_MODE_PREFERENCE_OPTIONS } from "../theme/dayModePreference";
+import {
+  DAY_MODE_PREFERENCE_OPTIONS,
+  type DayModePreference,
+} from "../theme/dayModePreference";
 import { useDayMode } from "../theme/fairycoreDayMode";
-import { THEME_OPTIONS } from "../theme/themeNames";
+import { THEME_OPTIONS, type ThemeName } from "../theme/themeNames";
 
 const DAY_MODE_PREFERENCE_ICONS = {
   time: TimeIcon,
@@ -46,44 +52,26 @@ const Themes = () => {
         <Heading size="lg">Theme</Heading>
       </Stack>
 
-      <Menu matchWidth>
-        <MenuButton
-          as={Button}
-          rightIcon={<ChevronDownIcon />}
-          borderWidth="2px"
-          borderColor="whiteAlpha.200"
-          borderRadius="1rem"
-          bg="whiteAlpha.100"
-          color={dayMode.isDay ? dayMode.textColor : undefined}
-          fontWeight="normal"
-          textAlign="left"
-          maxW="16rem"
-        >
-          {THEME_OPTIONS.find((option) => option.name === themeName)?.label}
-        </MenuButton>
-        <MenuList
-          minW="16rem"
-          borderRadius="1rem"
-          overflow="hidden"
-          color="white"
-        >
-          {THEME_OPTIONS.map((option) => (
-            <MenuItem
-              key={option.name}
-              onClick={() => setThemeName(option.name)}
-            >
-              {option.label}
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
+      <Select
+        display={{ base: "block", md: "none" }}
+        value={themeName}
+        onChange={(event) => setThemeName(event.target.value as ThemeName)}
+        borderWidth="2px"
+        borderColor="whiteAlpha.200"
+        borderRadius="1rem"
+        bg="whiteAlpha.100"
+        color={dayMode.isDay ? dayMode.textColor : undefined}
+        maxW="16rem"
+      >
+        {THEME_OPTIONS.map((option) => (
+          <option key={option.name} value={option.name}>
+            {option.label}
+          </option>
+        ))}
+      </Select>
 
-      <Stack spacing={3}>
-        <Stack spacing={1}>
-          <Heading size="md">Mode</Heading>
-        </Stack>
-
-        <Menu matchWidth>
+      <Box display={{ base: "none", md: "block" }}>
+        <Menu matchWidth autoSelect={false}>
           <MenuButton
             as={Button}
             rightIcon={<ChevronDownIcon />}
@@ -96,19 +84,7 @@ const Themes = () => {
             textAlign="left"
             maxW="16rem"
           >
-            <Stack direction="row" align="center" spacing={2}>
-              <Icon
-                as={DAY_MODE_PREFERENCE_ICONS[dayModePreference]}
-                boxSize={4}
-              />
-              <Text>
-                {
-                  DAY_MODE_PREFERENCE_OPTIONS.find(
-                    (option) => option.value === dayModePreference,
-                  )?.label
-                }
-              </Text>
-            </Stack>
+            {THEME_OPTIONS.find((option) => option.name === themeName)?.label}
           </MenuButton>
           <MenuList
             minW="16rem"
@@ -116,22 +92,116 @@ const Themes = () => {
             overflow="hidden"
             color="white"
           >
-            {DAY_MODE_PREFERENCE_OPTIONS.map((option) => (
-              <MenuItem
-                key={option.value}
-                icon={
-                  <Icon
-                    as={DAY_MODE_PREFERENCE_ICONS[option.value]}
-                    boxSize={4}
-                  />
-                }
-                onClick={() => setDayModePreference(option.value)}
-              >
-                {option.label}
-              </MenuItem>
-            ))}
+            {THEME_OPTIONS.map((option) => {
+              const isActive = option.name === themeName;
+
+              return (
+                <MenuItem
+                  key={option.name}
+                  onClick={() => setThemeName(option.name)}
+                  bg={isActive ? "whiteAlpha.200" : undefined}
+                  fontWeight={isActive ? "bold" : undefined}
+                  icon={
+                    isActive ? <CheckIcon boxSize={3} /> : <Box boxSize={3} />
+                  }
+                >
+                  {option.label}
+                </MenuItem>
+              );
+            })}
           </MenuList>
         </Menu>
+      </Box>
+
+      <Stack spacing={3}>
+        <Stack spacing={1}>
+          <Heading size="md">Mode</Heading>
+        </Stack>
+
+        <Select
+          display={{ base: "block", md: "none" }}
+          value={dayModePreference}
+          onChange={(event) =>
+            setDayModePreference(event.target.value as DayModePreference)
+          }
+          borderWidth="2px"
+          borderColor="whiteAlpha.200"
+          borderRadius="1rem"
+          bg="whiteAlpha.100"
+          color={dayMode.isDay ? dayMode.textColor : undefined}
+          maxW="16rem"
+        >
+          {DAY_MODE_PREFERENCE_OPTIONS.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </Select>
+
+        <Box display={{ base: "none", md: "block" }}>
+          <Menu matchWidth autoSelect={false}>
+            <MenuButton
+              as={Button}
+              rightIcon={<ChevronDownIcon />}
+              borderWidth="2px"
+              borderColor="whiteAlpha.200"
+              borderRadius="1rem"
+              bg="whiteAlpha.100"
+              color={dayMode.isDay ? dayMode.textColor : undefined}
+              fontWeight="normal"
+              textAlign="left"
+              maxW="16rem"
+            >
+              <Stack direction="row" align="center" spacing={2}>
+                <Icon
+                  as={DAY_MODE_PREFERENCE_ICONS[dayModePreference]}
+                  boxSize={4}
+                />
+                <Text>
+                  {
+                    DAY_MODE_PREFERENCE_OPTIONS.find(
+                      (option) => option.value === dayModePreference,
+                    )?.label
+                  }
+                </Text>
+              </Stack>
+            </MenuButton>
+            <MenuList
+              minW="16rem"
+              borderRadius="1rem"
+              overflow="hidden"
+              color="white"
+            >
+              {DAY_MODE_PREFERENCE_OPTIONS.map((option) => {
+                const isActive = option.value === dayModePreference;
+
+                return (
+                  <MenuItem
+                    key={option.value}
+                    bg={isActive ? "whiteAlpha.200" : undefined}
+                    fontWeight={isActive ? "bold" : undefined}
+                    icon={
+                      <Icon
+                        as={DAY_MODE_PREFERENCE_ICONS[option.value]}
+                        boxSize={4}
+                      />
+                    }
+                    onClick={() => setDayModePreference(option.value)}
+                  >
+                    <Stack
+                      direction="row"
+                      align="center"
+                      justify="space-between"
+                    >
+                      <Text>{option.label}</Text>
+                      {isActive && <CheckIcon boxSize={3} />}
+                    </Stack>
+                  </MenuItem>
+                );
+              })}
+            </MenuList>
+          </Menu>
+        </Box>
         <Text fontSize="xs" opacity={0.85}>
           {
             DAY_MODE_PREFERENCE_OPTIONS.find(
